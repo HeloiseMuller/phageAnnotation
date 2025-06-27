c3f130beb3378b491fe24dc16246c9dca10eeb15
Héloïse Muller <heloise.muller@laposte.net>
2025-06-27 12:47:39 -0400

    complete 1st step

--- phageAnnotation_array.sh
+++ phageAnnotation_array.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+#Argument 1 = array number
+#Argument 2 = genomesPath
+#Argument 3 = outputsPath
+#Argument 4 = path to the file dataset.lst
+#Argument 5 = path of checkV database
+
 source /network/rit/lab/andamlab/bin/miniconda3/etc/profile.d/conda.sh
 conda activate virsorter2
 umask g+rwx
 
 #Get the th line of the file dataset.lst
-#assembly=`sed "$1q;d" dataset_933.lst`
-assembly=`sed "$1q;d" new458_update_27thMay2025.lst`
+assembly=`sed "$1q;d" $4`
 
-cd /network/rit/lab/andamlab/Heloise/project_actinophages/viralAnnotation/
+cd $3
 
-#input=`ls /network/rit/lab/andamlab/Heloise/project_Fus/dataset/genomes/${assembly}*_genomic.fna`
-input=`ls /network/rit/lab/andamlab/Heloise/project_actinophages/dataset/genomes_notIn_project_Fus/${assembly}*_genomic.fna`
+input=`ls $2/${assembly}*_genomic.fna`
 
 virsorter run --keep-original-seq -i  $input -w $assembly/vs2-pass1/ -j 18 all
 
 #All means run the all pipeline
 
 #Add assembly name to files
-mv  $assembly/vs2-pass1/final-viral-boundary.tsv $assembly/vs2-pass1/${assembly}_final-viral-boundary.tsv
+mv $assembly/vs2-pass1/final-viral-boundary.tsv $assembly/vs2-pass1/${assembly}_final-viral-boundary.tsv
 mv $assembly/vs2-pass1/final-viral-combined.fa $assembly/vs2-pass1/${assembly}_final-viral-combined.fa
 mv $assembly/vs2-pass1/final-viral-score.tsv $assembly/vs2-pass1/${assembly}_final-viral-score.tsv
 
@@ -28,7 +32,7 @@ awk -v var="$assembly" '{print $0, var}' OFS='\t' $assembly/vs2-pass1/${assembly
 sed "s/>/>${assembly}:/g"  $assembly/vs2-pass1/${assembly}_final-viral-combined.fa > $assembly/vs2-pass1/${assembly}_final-viral-combined_named.fa
 
 conda activate checkV
-checkv end_to_end $assembly/vs2-pass1/${assembly}_final-viral-combined_named.fa $assembly/checkv -t 18 -d /network/rit/lab/andamlab/Databases/checkv-db-v1.5/
+checkv end_to_end $assembly/vs2-pass1/${assembly}_final-viral-combined_named.fa $assembly/checkv -t 18 -d $5
 conda deactivate
 
 cat $assembly/checkv/proviruses.fna $assembly/checkv/viruses.fna > $assembly/checkv/${assembly}_combined.fna
@@ -39,5 +43,3 @@ awk -v var="$assembly" '{print $0, var}' OFS='\t' $assembly/checkv/contamination
 virsorter run --seqname-suffix-off --viral-gene-enrich-off --provirus-off --prep-for-dramv -i $assembly/checkv/${assembly}_combined.fna -w $assembly/vs2-pass2 -j 18 all
 mv $assembly/vs2-pass2/final-viral-combined.fa $assembly/vs2-pass2/${assembly}_final-viral-combined.fa
 mv $assembly/vs2-pass2/final-viral-score.tsv $assembly/vs2-pass2/${assembly}_final-viral-score.tsv
-
-
--- phageAnnotation_main.sh
+++ phageAnnotation_main.sh
@@ -3,14 +3,10 @@
 #SBATCH --partition=batch
 #SBATCH --cpus-per-task=18
 #SBATCH --mem=64G
-#SBATCH -o /network/rit/lab/andamlab/Heloise/project_actinophages/viralAnnotation/annotVirus-slurm-%j.out
+#SBATCH -o /network/rit/lab/andamlab/Heloise/project_actinophages/viralAnnotation/phageAnnotation-slurm-%j.out
 #SBATCH -D /network/rit/lab/andamlab/Heloise/project_actinophages/  # working directory
 #SBATCH -v
 
 #SBATCH --array=3-458%4
 
-
-bash scripts/run_annotVirus_array.sh $SLURM_ARRAY_TASK_ID
-
-
-
+bash phageAnnotation_array.sh $SLURM_ARRAY_TASK_ID genomesPath outputsPath dataset.lst pathDB
